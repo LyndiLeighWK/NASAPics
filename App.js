1fc282bd0a1015d760b7f63fc0d00aa525cbd324
LyndiLeighWK <lyndi.weener-kuiper@odb.org>
2018-07-16 18:32:19 -0400

    Removing dummy data

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import {StyleSheet, Text, View} from 'react-native';
 import HomeScreen from './src/components/HomeScreen';
 
 const App = () => {
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, Image, View, ScrollView} from 'react-native';
+import {Text,View,Image,ScrollView} from 'react-native';
 import axios from 'axios';
 import moment from 'moment';
 
@@ -10,40 +10,54 @@ import Header from './Header';
 import Button from './Button';
 
 class HomeScreen extends React.Component {
-    state = { picOfTheDay: [ ] };
+    constructor(props){
+        super(props);
+        this.state = {
+            picOfTheDay: {
+                date : new Date()
+            },
+        };
+    }
 
     componentWillMount() {
-        // axios.get('https://api.nasa.gov/planetary/apod?api_key=IJ7ZRsSIGvLapSPjhfojEmWhBNyejCYN9F1qJQjL')
-        // .then(response => this.setState({picOfTheDay: response.data}))
-        // .catch(error => { console.log('caught', error) });
-        var defaultPic = {
-            url : 'https://apod.nasa.gov/apod/image/1807/Capture13_07_2018_2_09_09AM_PK1024.jpg',
-            title : 'A Nibble on the Sun',
-            copyright : 'Padraic Koen',
-            date : '2018-07-11',
-            explanation : 'The smallest of the three partial solar eclipses during 2018 was just yesterday, Friday, July 13. It was mostly visible over the open ocean between Australia and Antarctica. Still, this video frame of a tiny nibble on the Sun was captured through a hydrogen-alpha filter from Port Elliott, South Australia, during the maximum eclipse visible from that location. There, the New Moon covered about 0.16 percent of the solar disk. The greatest eclipse, about one-third of the Sun\'s diameter blocked by the New Moon, could be seen from East Antarctica near Peterson Bank, where the local emperor penguin colony likely had the best view. During this prolific eclipse season, the coming Full Moon will bring a total lunar eclipse on July 27, followed by yet another partial solar eclipse at the next New Moon on August 11.'
-        }
-        this.setState({picOfTheDay: defaultPic});
+        //Fetch Today's Content
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=IJ7ZRsSIGvLapSPjhfojEmWhBNyejCYN9F1qJQjL')
+        .then(response => this.setState({picOfTheDay: response.data}))
+        .catch(error => { console.log('caught', error.message) });
+
+        // var defaultPic = {
+        //     url : 'https://apod.nasa.gov/apod/image/1807/Capture13_07_2018_2_09_09AM_PK1024.jpg',
+        //     title : 'A Nibble on the Sun',
+        //     copyright : 'Padraic Koen',
+        //     date : '2018-07-15',
+        //     explanation : 'The smallest of the three partial solar eclipses during 2018 was just yesterday, Friday, July 13. It was mostly visible over the open ocean between Australia and Antarctica. Still, this video frame of a tiny nibble on the Sun was captured through a hydrogen-alpha filter from Port Elliott, South Australia, during the maximum eclipse visible from that location. There, the New Moon covered about 0.16 percent of the solar disk. The greatest eclipse, about one-third of the Sun\'s diameter blocked by the New Moon, could be seen from East Antarctica near Peterson Bank, where the local emperor penguin colony likely had the best view. During this prolific eclipse season, the coming Full Moon will bring a total lunar eclipse on July 27, followed by yet another partial solar eclipse at the next New Moon on August 11.'
+        // }
+        // this.setState({
+        //     picOfTheDay: defaultPic,
+        // });
     };
 
     onNavigateForward(){
         // Add day to current state date
-        var nextDate = moment(new Date(this.state.picOfTheDay.date)).add(1, 'day').format("YYYY-MM-DD");
+        var nextDate = new moment(this.state.picOfTheDay.date).add(1, 'day').format("YYYY-MM-DD");
         // Fetch new content
         this.navigateToNewDay(nextDate);
     };
 
     onNavigateBackward() {
         // Subtract day to current state date
-        var previousDate = moment(this.state.picOfTheDay.date).subtract(1, 'day').format("YYYY-MM-DD");
+        var previousDate = new moment(this.state.picOfTheDay.date).subtract(1, 'day').format("YYYY-MM-DD");
         // Fetch new content
         this.navigateToNewDay(previousDate);
-    }
+    };
 
     formatDate() {
-        var date = moment(new Date(this.state.picOfTheDay.date)).format("MMMM D, YYYY");
-        return date.toString();
-    }
+        return moment(this.state.picOfTheDay.date, "YYYY-MM-DD").format("MMMM D, YYYY");
+    };
+
+    checkIfForwardNavAvailable() {
+        return moment().format("MMMM D, YYYY") == moment(this.state.picOfTheDay.date, "YYYY-MM-DD").format("MMMM D, YYYY");
+    };
 
     navigateToNewDay(newDay){
         // Fetch new content
@@ -51,7 +65,7 @@ class HomeScreen extends React.Component {
         // Update state
         .then(response => this.setState({picOfTheDay: response.data}))
         .catch(error => { console.log('caught', error.message) });
-    }
+    };
 
     render() {
         return (
@@ -61,16 +75,18 @@ class HomeScreen extends React.Component {
                     <Card>
                         <CardSection>
                             <Button onPress={() => this.onNavigateBackward()}>&lt;&lt;</Button>
-                            {/* <Text>{this.state.picOfTheDay.date}</Text> */}
-                            <Text style={Styles.dateStyle}>{this.formatDate(this.state.picOfTheDay.date)}</Text>
-                            <Button onPress={() => this.onNavigateForward()}>&gt;&gt;</Button>
+                            <Text style={Styles.dateStyle}> { this.formatDate(this.state.picOfTheDay.date)}</Text>
+                            <Button onPress={() => this.onNavigateForward()}>
+                                {/* Only display forward button if current date is not today */}
+                                { this.checkIfForwardNavAvailable() ? null : '>>' }
+                            </Button>
                         </CardSection>
                         <Image
                             style={Styles.imageStyle}
                             source={{uri: this.state.picOfTheDay.url}}
                         />
                         <CardSection>
-                            <Text>{'Copyright: ' + this.state.picOfTheDay.copyright}</Text>
+                            { this.state.picOfTheDay.copyright != null ? <Text>{'Copyright : ' + this.state.picOfTheDay.copyright}</Text> : null}
                         </CardSection>
                         <CardSection>
                             <Text style={Styles.titleStyle}>{this.state.picOfTheDay.title}</Text>
